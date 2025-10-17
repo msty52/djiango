c070a87909d485b2b7b549ce1449168f03d96364
msty52 <89525bobbob@gmail.com>
2025-10-17 03:17:05 +0300

    Add files via upload

---
+++ myproject/chat/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/<int:room_id>/', views.room_detail, name='room_detail'),
+]
