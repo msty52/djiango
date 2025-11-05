f134b015fc6fbdcbce26b45b2a0918fd6f6ff771
msty52 <89525bobbob@gmail.com>
2025-11-05 16:16:07 +0300

    Delete myproject directory

--- myproject/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('room/<int:room_id>/', views.room_detail, name='room_detail'),
-]
